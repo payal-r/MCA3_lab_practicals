9edc33302bbc3b7893b988e134c239121f1b30fd
Bhartendu-Pant <81458136+Bhartendu-Pant@users.noreply.github.com>
2021-10-10 15:33:00 +0530

    Update Q4.py

--- MCA3A/Bhartendu_Pant_20711020_MCA3A/Q4.py
+++ MCA3A/Bhartendu_Pant_20711020_MCA3A/Q4.py
@@ -1,2 +1,13 @@
-import math
-print("The value of pi is", math.pi)
+num=int(input("Input any number :"))
+flag=False
+
+if num > 1:     # prime numbers are always greater than 1
+    for i in range(2,num):
+        if(num % i)==0:
+            flag=True
+            break
+if(flag):
+    print(num, " is not a prime number")
+else:
+    print(num, " is a prime number")
+
