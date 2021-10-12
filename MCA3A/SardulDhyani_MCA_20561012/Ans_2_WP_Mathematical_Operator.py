8cd523ad8d1381dd776df927d7f94e9bb30791b0
Sardul Dhyani <65482324+SardulDhyani@users.noreply.github.com>
2021-10-12 11:37:38 +0530

    Code Correction

--- MCA3A/SardulDhyani_MCA_20561012/Ans_2_WP_Mathematical_Operator.py
+++ MCA3A/SardulDhyani_MCA_20561012/Ans_2_WP_Mathematical_Operator.py
@@ -1,5 +1,8 @@
 value1 = 2
 value2 = 3
 
-ans = value1 + value2
-print(ans)
+print("Add :", value1 + value2)
+print("Difference :", value1 - value2)
+print("Product :", value1 * value2)
+print("Divison :", value1 / value2)
+print("Modulas :", value1 % value2)
