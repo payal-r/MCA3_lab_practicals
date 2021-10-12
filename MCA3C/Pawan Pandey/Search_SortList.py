af8ab20542750714679acb117d376c681651f502
pawan007-maker <85864480+pawan007-maker@users.noreply.github.com>
2021-10-12 03:22:48 -1100

    Add files via upload

---
+++ MCA3C/Pawan Pandey/Search_SortList.py
@@ -0,0 +1,9 @@
+str = [70, 15, 18, 38, 26]
+print(sorted(str))  # with sorting
+print(str)  # without sorting
+
+index = str.index(38)
+print(index)
+
+index = str.index(26)
+print(index)
---
+++ MCA3C/Pawan Pandey/tuple_usage.py
@@ -0,0 +1,7 @@
+tuple1 = ("Pawan", 22, "Male")
+print(tuple1)
+
+tuple1 = ("GEU", "GEHU_DEHRADUN", "GEHU_DEEHRADUN")
+tuple2 = (1, 2, 3)
+print(tuple1)
+print(tuple2)
